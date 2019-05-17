1aa6c29413fbbe8b2bafd73399af7b4c740da964
dhondiramjawade <dhoondiramjawade@gmail.com>
2019-05-18 02:28:04 +0530

    filter impl

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -35,7 +35,7 @@ export class DashboardComponent implements OnInit {
           }
         },
         showInLegend : true
-      } 
+      }
     },
     series: [{
       type: 'pie',
--- src/app/temp/temp.component.ts
+++ src/app/temp/temp.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import {MatSidenavModule } from '@angular/material/sidenav';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState,
+} from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-temp',
@@ -6,10 +14,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./temp.component.css']
 })
 export class TempComponent implements OnInit {
-  heroes = ["abc","ss","ssss"];
-  constructor() { }
+  @ViewChild('MatSidenavModule') MatSidenavModule: any;
+// tslint:disable-next-line: max-line-length
+  public  isLargeScreen: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(map((result: BreakpointState) => result.matches));
+  openDrawer: boolean;
+  heroes = ['abc', 'ss', 'ssss'];
+  constructor(
+    private breakpointObserver: BreakpointObserver
+  ) { }
 
   ngOnInit() {
+    this.openDrawer = true;
+
+  }
+
+  toggleFilter (): void  {
+    this.openDrawer = ! this.openDrawer;
   }
 
 }
