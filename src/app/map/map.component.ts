cbef1f188ea7cce0eabb5f36eea92cc179bbe7fe
dhondiramjawade <dhondiramjawade@gamil.com>
2019-06-13 22:04:57 +0530

    map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 // import { LeafletModule } from "@asymmetrik/ngx-leaflet";
 import * as L from 'leaflet';
+import * as createjs from "createjs-module";
 import { icon, latLng, Map, marker, point, polyline, tileLayer } from 'leaflet';
 
 @Component({
@@ -9,16 +11,20 @@ import { icon, latLng, Map, marker, point, polyline, tileLayer } from 'leaflet';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+
   baseLayers = [ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18  }) ];
   center = L.latLng([ 21.616579336740603,  78.57421875 ]);
   fitBounds = L.latLngBounds([ [41.902277040963696, 144.14062500000003], [0.7031073524364909, 28.212890625000004] ]);
   
   onMapReady(map: Map) {
     map.setZoom(4);
+    var x = createjs;
   }
   constructor() {  
     // var sampleMap = L.map('mapid').setView([0, 0], 13);
     // this.baseLayers = [ L.tileLayer( 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', { maxZoom: 18 }) ];
   }
-  ngOnInit() {}
+  ngOnInit() {
+    
+  }
 }
