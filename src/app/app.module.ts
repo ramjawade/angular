3fc0fb61e2f269ebd3fa704010156c4cc424730e
dhondiramjawade <dhondiramjawade@gmail.com>
2019-09-25 17:49:43 +0530

    added comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,21 @@ import { PdfViewerModule } from "ng2-pdf-viewer";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { AgGridModule} from 'ag-grid-angular';
-
-import { FourZeroFourComponent } from './four-zero-four/four-zero-four.component';
+/**
+ * Material Module
+ * Privide all Material Component
+ */
 import { MaterialModuleModule } from './material-module/material-module.module';
+/**
+ * App Routing Module
+ */
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
+/**
+ * Error Component
+ */
+import { FourZeroFourComponent } from './four-zero-four/four-zero-four.component';
 
 @NgModule({
   declarations: [
