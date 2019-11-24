acf1366c8a3ec4e3d67b4e9c4444efc1ffc1961d
dhondiramjawade <dhondiramjawade@gmail.com>
2019-11-24 17:38:16 +0530

    google mp

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { icon, latLng, Map, marker, point, polyline, tileLayer , gridLayer } from 'leaflet';
-import * as createjs from 'createjs-module';
+import { icon, latLng, Map, marker, point, polyline, gridLayer, latLngBounds } from 'leaflet';
+import 'leaflet.gridlayer.googlemutant/Leaflet.GoogleMutant';
 
-
-declare let L;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -11,23 +9,19 @@ declare let L;
 })
 export class MapComponent implements OnInit {
   public map: any;
-  // baseLayers = [ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18  }) ];
-  center = L.latLng([ 21.616579336740603,  78.57421875 ]);
-  fitBounds = L.latLngBounds([ [41.902277040963696, 144.14062500000003], [0.7031073524364909, 28.212890625000004] ]);
+  center = latLng([ 21.616579336740603,  78.57421875 ]);
+  fitBounds = latLngBounds([ [41.902277040963696, 144.14062500000003], [0.7031073524364909, 28.212890625000004] ]);
 
   constructor(
-    // private leafletDirective: LeafletDirective
   ) {}
   ngOnInit() {
   }
 
   onMapReady(map: Map) {
-    L.gridLayer.googleMutant({
+    gridLayer.googleMutant({
       type: 'roadmap'	// valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
     }).addTo(map);
     this.map = map;
     map.setZoom(4);
-    const x = createjs;
-    // const y = this.leafletDirective;
    }
 }
